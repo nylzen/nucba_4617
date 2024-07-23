6858ddefbfd03851f26f800f254f8eee86b7b1d2
Nylzen <tugoresn@gmail.com>
2024-07-22 21:36:34 -0300

    Intro JS

---
+++ Javascript/Clase_01/main.js
@@ -0,0 +1,144 @@
+// console.log('Hola desde otro archivo')
+// let messi = "Messi" // String
+// console.log(messi)
+
+/*
+  Tipos de datos que tenemos:
+  - string: 'Ejemplo', "Ejemplo", `Ejemplo`
+  - boolean: true / false
+  - number: 12341234, 12.1, NaN, Infinity
+  - undefined: Algo no definido
+  - null: vacio
+*/
+
+// console.log('Algo "algo"')
+// let o const
+// let nombre = 'Messi'
+// console.log(`Hola soy ${nombre}`)
+// console.log('Hola soy ' + nombre)
+
+// console.log(123123)
+// console.log(-123123)
+// console.log(12.1)
+// console.log(12, 1)
+// console.log(NaN)
+// console.log(Infinity)
+
+// let nombreUsuario = ''
+// let nombreUsuario = prompt('Ingresa nombre')
+// console.log(nombreUsuario)
+
+// variables
+// var nombreVariable = 'Messi'
+// console.log(nombreVariable)
+// var nombreVariable = 10
+// console.log(nombreVariable)
+
+//  Let
+// SE declara con la palabra reservada let, no podemos re-declarar pero si podemos cambiar el valor
+// let nombreVariable = 'tuki'
+
+// console.log(nombreVariable)
+// nombreVariable = 'messi'
+// console.log(nombreVariable)
+// let nombre // Declaramos una variable
+// nombre = 'algo' // Asignamos un valor
+// console.log(nombre)
+
+// Const
+// const, no podemos re-declarar ni cambiar el valor y no podemos inicializarla sin un valor
+
+// const nombreConstante = 'Messi campeon del mundo'
+// const algo
+// console.log(nombreConstante)
+// nombreConstante = 'Mesi no es campeon'
+
+
+let numberA = 10
+let numberB = 20
+let result = numberA + numberB
+
+// console.log(result)
+
+let textoA = 'Boca'
+let textoB = 'Juniors'
+
+// console.log(textoA + " " + textoB)
+// console.log(`${textoA} otra cosa ${textoB}`)
+
+// console.log(`El nombre del club mas grande es ${textoA} ${textoB}`)
+// console.log('El nomrbe del club es: ' + textoA + " " + textoB)
+
+// Operadores
+/*
+  + sumar
+  - restar
+  / dividir
+  * multiplicar
+  % resto
+  ++ incrementar
+  -- decrementar
+*/
+
+// Sumar
+// console.log(numberA + numberB)
+
+// resta
+// console.log(numberA - numberB)
+
+// multiplicacion
+// console.log(numberA * numberB)
+
+// division
+// console.log(numberA / numberB)
+
+// resto
+// console.log(numberB % 2)
+
+// incrementar
+// console.log(numberA)
+// numberA++
+// numberA = numberA + 1
+// console.log(numberA)
+
+// decrementar
+// console.log(numberA)
+// numberA--
+// console.log(numberA--)
+// console.log(numberA)
+
+
+// Prompt: Es una funcion para abrir una ventanita para que el usuario ingrese un valor
+// prompt('Tu nombre?') No se guardo en ningun lado, entonces?
+
+let nombre = prompt('Tu nombre?')
+console.log(nombre)
+
+
+// Pseudocodigo
+/*
+  - Si el usuario no escribe nada y le da cancelar me devuelve vacio
+  - Preguntamos si lo que devuelve el prompt es null => vacio vamos a querer imprimir por consola que esta vacio
+  - Si el usuario escribe algo quiero mostrar ese algo
+*/
+
+// if(nombre === null) {
+//   console.log('Tas vacio')
+// } else {
+//   console.log('Tu nombre es: ' + nombre)
+// }
+
+// let arr = [1,2,3,4,5,6,7]
+// console.log(arr)
+
+// let persona = {
+//   nombre: 'Nelson'
+// }
+
+// console.log(persona)
+
+// let ejemplo = [1, true, 'Messi', {nombre: 'Nelson'}]
+// console.log(ejemplo)
+
+// Extensiones: ES7+ React/Redux...
+// Error Lens
