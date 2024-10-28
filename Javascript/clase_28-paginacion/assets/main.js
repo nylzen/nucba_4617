697871bb9089d965c7c26636bd513a302b57307a
Nelson <tugoresn@gmail.com>
2024-10-28 19:53:24 -0300

    clase 28 starter

---
+++ Javascript/clase_28-paginacion/assets/main.js
@@ -0,0 +1,4 @@
+/* Las imágenes de movieDB vienen con una URL de base la cual se tiene que colocar en conjunto con lo que venga en el objeto de cada película para que la imagen aparezca. Guardamos esta en una constante. 
+Para mas referencia :  https://developers.themoviedb.org/3/getting-started/images . Elegimos para esta APP el tamaño original.
+*/
+const ImgBaseUrl = "https://image.tmdb.org/t/p/original";
